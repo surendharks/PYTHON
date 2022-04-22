c02bfb22e95007694462b0b88b321ee8f79e46b4
surendharks <80706696+surendharks@users.noreply.github.com>
2022-04-22 19:39:47 +0530

    done

---
+++ Setoperation.py
@@ -0,0 +1,12 @@
+#program to perform the various set operations 
+
+s1={0,1,2,3,4,5,6}
+s2={2,4,8}
+
+print(s1.union(s2)) #union
+
+print(s1.intersection(s2)) #intersection
+
+print(s1.difference(s2)) #difference
+
+print(s1.symmetric_difference(s2)) #symmetric_difference
